b9e4be25632dfe8ad4f2195707910d2c40f1fce5
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2019-09-18 13:56:55 -0400

    Add flags to main file

--- src/main.cc
+++ src/main.cc
@@ -1,23 +1,31 @@
 #include <iostream>
+#include <string>
 
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/flags/usage.h"
+#include "jack/jack.h"
 
+using std::cerr;
 using std::cout;
 using std::endl;
+using std::string;
 
-ABSL_FLAG(bool, enabled, false, "Enable the flag");
+ABSL_FLAG(string, clientName, "jack-aec", "Client name to use when connecting to JACK server.");
+ABSL_FLAG(string, serverName, "", "Server name to connect to. Leave empty for default server.");
 
 int main(int argc, char** argv){
-    absl::SetProgramUsageMessage("A simple demo for absl/flags");
+    absl::SetProgramUsageMessage("A JACK application for acoustic echo cancellation.");
     absl::ParseCommandLine(argc, argv);
-    bool enabled = absl::GetFlag(FLAGS_enabled);
-    if (enabled){
-        cout << "Enabled" << endl;
-    } else {
-        cout << "Disabled" << endl;
+
+    const char* clientName = absl::GetFlag(FLAGS_clientName).c_str();
+    const char* serverName = NULL;
+    const string serverNameString = absl::GetFlag(FLAGS_serverName);
+    if (!serverNameString.empty()) {
+        serverName = serverNameString.c_str();
     }
+    jack_options_t options = JackNullOption;
+
     return 0;
 }
 
